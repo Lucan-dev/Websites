1a7f24e894b871d6c72646ef7ea25f0917ee8454
Lucan <68242429+Lucan-dev@users.noreply.github.com>
2020-09-10 16:36:43 -0700

    Finished chat app

--- WebSockets/index.js
+++ WebSockets/index.js
@@ -9,11 +9,21 @@ var server = app.listen(4000, function() {
 });
 
 // Static files
-app.use(express.static('public'));
+app.use(express.static('./'));
 
 // Socket setup
 var io = socket(server);
 
 io.on('connection', function(socket) {
-    console.log('Made socket connection.')
+    console.log('Made socket connection id:', socket.id)
+
+    // Handle chat event
+    socket.on('chat', function(data) {
+        io.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(handle) {
+        socket.broadcast.emit('typing', handle);
+    });
 });
