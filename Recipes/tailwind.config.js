493ac85bdbe2b0bda3ffc96da7c7ac4c63aaaaa6
Lucan <68242429+Lucan-dev@users.noreply.github.com>
2020-09-11 17:16:19 -0700

    Finished recipe app

--- Recipes/tailwind.config.js
+++ Recipes/tailwind.config.js
@@ -6,8 +6,11 @@ module.exports = {
         primary: '#ff6363',
         secondary: {
           100: '#e2e2d5',
-          500: '#888883',
+          200: '#888883',
         }
+      },
+      fontFamily: {
+        body: ['Nunito']
       }
     },
   },
